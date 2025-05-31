6093d331a68977a039c36b52697b8ac1523adc7e
Raz Nissim <raznissim@gmail.com>
2025-05-31 17:37:18 +0300

    up to chapter 6

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,21 @@
+'use client';
+// This file defines the navigation links for the dashboard.
+// It uses Next.js's Link component for client-side navigation.
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
+import Link from 'next/link';
+
+import { usePathname } from 'next/navigation';
+// This hook is used to get the current pathname.
+// It can be used to highlight the active link in the navigation.
+
+import clsx from 'clsx';
+
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +30,27 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+  // This hook is used to get the current pathname.
+  // It can be used to highlight the active link in the navigation.
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
+            >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
